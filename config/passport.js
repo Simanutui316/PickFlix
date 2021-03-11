2ec88b030ac2585e873316d50c53fdf9988cd5dd
Justin TuiSamoa <justintuisamoa@Justins-MacBook-Pro.local>
2021-03-11 10:06:17 -0800

    test commit

---
+++ config/passport.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//Require your User Model here!
+
+// configuring Passport!
+// this gets called at the initial login
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK
+},
+  function (accessToken, refreshToken, profile, cb) {
+    // a user has logged in via OAuth!
+
+  }
+));
+
+// this puts user id in the session cookie
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+
+  // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
+  // When you call this done function passport assigns the user document to req.user, which will 
+  // be availible in every Single controller function, so you always know the logged in user
+
+});
+
+
+
